af20c4ee822edfedd42b1be5c7ee8ddda61eeb3b
Ajit Brar <4261648+brarajit18@users.noreply.github.com>
2024-05-18 10:40:24 -0400

    Updated rqiurements file

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/ask'
+url = 'http://18.206.219.103/ask'
 headers = {'Content-Type': 'application/json'}
 data = {'question': 'What are pure functions in javascript?'}
 
